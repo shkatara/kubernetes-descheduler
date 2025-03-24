31988d85bfaa4af5672464086bf9137aa12c0e64
Shubham Katara <shubham.katara@trivago.com>
2025-03-24 20:26:13 +0100

    Delete pods that are running on spot nodes

--- main.go
+++ main.go
@@ -35,24 +35,24 @@ func main() {
 
 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	for _, pod := range pods.Items {
-		pods_custom[pod.GetName()] = map[string]string{"HostIP": pod.Status.HostIP, "Spot": pod.Labels["spot"]}
+		pods_custom[pod.GetName()] = map[string]string{"HostIP": pod.Status.HostIP, "NodeName": pod.Spec.NodeName, "CreationTimestamp": pod.GetCreationTimestamp().String(), "Namespace": pod.GetNamespace()}
 	}
 
 	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
-
 	for _, node := range nodes.Items {
 		if node.GetLabels()["is_spot"] == "true" {
-			fmt.Println("The node is a spot instance -> ", node.GetName())
 			list_of_spot_instances[node.GetName()] = map[string]string{"HostIP": node.Status.Addresses[0].Address}
 		}
 	}
 
-	fmt.Println(list_of_spot_instances)
-
 	for k, v := range pods_custom {
-		for _, v1 := range list_of_spot_instances {
-			if v["HostIP"] == v1["HostIP"] {
-				fmt.Println("The pod should be deleted and put on spot -> ", k)
+		for _, value := range list_of_spot_instances {
+			if v["HostIP"] == value["HostIP"] {
+				fmt.Println("Deleting", k, "now")
+				err = clientset.CoreV1().Pods(v["Namespace"]).Delete(context.TODO(), k, metav1.DeleteOptions{})
+				if err != nil {
+					panic(err.Error())
+				}
 			}
 		}
 	}
