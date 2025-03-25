727ae39dbaf8fd2f3080253f536c66aaf79e4140
Shubham Katara <shubham.katara@trivago.com>
2025-03-25 10:23:09 +0100

    Place pods on spot nodes after a spot node is recreated

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"slices"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -11,8 +12,9 @@ import (
 )
 
 var (
-	pods_custom            = make(map[string]map[string]string)
-	list_of_spot_instances = make(map[string]map[string]string)
+	pods_custom           = make(map[string]map[string]string)
+	map_of_spot_instances = make(map[string]map[string]string)
+	list_of_spot_ips      = make([]string, 0)
 )
 
 func main() {
@@ -34,25 +36,37 @@ func main() {
 	}
 
 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	// find pods that have a node with affinity to spot instances
 	for _, pod := range pods.Items {
-		pods_custom[pod.GetName()] = map[string]string{"HostIP": pod.Status.HostIP, "NodeName": pod.Spec.NodeName, "CreationTimestamp": pod.GetCreationTimestamp().String(), "Namespace": pod.GetNamespace()}
+		if pod.Spec.Affinity != nil && pod.Spec.Affinity.NodeAffinity != nil {
+			for _, term := range pod.Spec.Affinity.NodeAffinity.PreferredDuringSchedulingIgnoredDuringExecution {
+				for _, expr := range term.Preference.MatchExpressions {
+					if expr.Key == "is_spot" && slices.Contains(expr.Values, "true") {
+						fmt.Printf("Pod %s has node affinity to spot instances\n", pod.GetName())
+						pods_custom[pod.GetName()] = map[string]string{"HostIP": pod.Status.HostIP, "NodeName": pod.Spec.NodeName, "CreationTimestamp": pod.GetCreationTimestamp().String(), "Namespace": pod.GetNamespace()}
+					}
+				}
+			}
+		}
 	}
 
 	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 	for _, node := range nodes.Items {
 		if node.GetLabels()["is_spot"] == "true" {
-			list_of_spot_instances[node.GetName()] = map[string]string{"HostIP": node.Status.Addresses[0].Address}
+			map_of_spot_instances[node.GetName()] = map[string]string{"HostIP": node.Status.Addresses[0].Address}
 		}
 	}
 
-	for k, v := range pods_custom {
-		for _, value := range list_of_spot_instances {
-			if v["HostIP"] == value["HostIP"] {
-				fmt.Println("Deleting", k, "now")
-				err = clientset.CoreV1().Pods(v["Namespace"]).Delete(context.TODO(), k, metav1.DeleteOptions{})
-				if err != nil {
-					panic(err.Error())
-				}
+	// convert map_of_spot_instances to slice
+	for _, v := range map_of_spot_instances {
+		list_of_spot_ips = append(list_of_spot_ips, v["HostIP"])
+	}
+
+	for k, podinfo := range pods_custom {
+		if !slices.Contains(list_of_spot_ips, podinfo["HostIP"]) {
+			err = clientset.CoreV1().Pods(podinfo["Namespace"]).Delete(context.TODO(), k, metav1.DeleteOptions{})
+			if err != nil {
+				panic(err.Error())
 			}
 		}
 	}
