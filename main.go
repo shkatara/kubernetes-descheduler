5b32da161b004efe1d36456daaeee9984f94c480
Shubham Katara <shubham.katara@trivago.com>
2025-03-25 15:07:02 +0100

    Add condition to only select READY pods and the ones that are older than 10 minutes from the time the job runs

--- main.go
+++ main.go
@@ -12,11 +12,24 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+type PodName = string
+type NodeName = string
+
 var (
-	pods_with_affinity    = make(map[string]map[string]string)
-	map_of_spot_instances = make(map[string]map[string]string)
+	pods_with_affinity    = make(map[PodName]PodWithAffinity)
+	map_of_spot_instances = make(map[NodeName]SpotInstance)
 	list_of_spot_ips      = make([]string, 0)
 )
+
+type PodWithAffinity struct {
+	HostIP            string
+	CreationTimestamp time.Time
+	Namespace         string
+}
+
+type SpotInstance struct {
+	HostIP string
+}
 
 func main() {
 	start := time.Now()
@@ -39,6 +52,7 @@ func main() {
 	}
 	// find pods that have a node with affinity to spot instances
 	for _, pod := range pods.Items {
+
 		if pod.Spec.Affinity != nil && pod.Spec.Affinity.NodeAffinity != nil {
 			for _, term := range pod.Spec.Affinity.NodeAffinity.PreferredDuringSchedulingIgnoredDuringExecution {
 				for _, expr := range term.Preference.MatchExpressions {
@@ -48,7 +62,7 @@ func main() {
 						for _, condition := range pod.Status.Conditions {
 							if condition.Type == "Ready" {
 								if condition.Status == "True" {
-									pods_with_affinity[pod.GetName()] = map[string]string{"HostIP": pod.Status.HostIP, "NodeName": pod.Spec.NodeName, "CreationTimestamp": pod.GetCreationTimestamp().String(), "Namespace": pod.GetNamespace()}
+									pods_with_affinity[pod.GetName()] = PodWithAffinity{HostIP: pod.Status.HostIP, CreationTimestamp: pod.GetCreationTimestamp().Time, Namespace: pod.GetNamespace()}
 								}
 							}
 						}
@@ -64,20 +78,22 @@ func main() {
 	}
 	for _, node := range nodes.Items {
 		if node.GetLabels()["cloud.google.com/gke-spot"] == "true" {
-			map_of_spot_instances[node.GetName()] = map[string]string{"HostIP": node.Status.Addresses[0].Address}
+			map_of_spot_instances[node.GetName()] = SpotInstance{HostIP: node.Status.Addresses[0].Address}
 		}
 	}
 
 	// convert map_of_spot_instances to slice
 	for _, v := range map_of_spot_instances {
-		list_of_spot_ips = append(list_of_spot_ips, v["HostIP"])
+		list_of_spot_ips = append(list_of_spot_ips, v.HostIP)
 	}
 
 	for k, podinfo := range pods_with_affinity {
-		if !slices.Contains(list_of_spot_ips, podinfo["HostIP"]) {
-			err = clientset.CoreV1().Pods(podinfo["Namespace"]).Delete(context.TODO(), k, metav1.DeleteOptions{})
-			if err != nil {
-				panic(err.Error())
+		if podinfo.CreationTimestamp.Before(time.Now().Add(10 * time.Minute)) {
+			if !slices.Contains(list_of_spot_ips, podinfo.HostIP) {
+				err = clientset.CoreV1().Pods(podinfo.Namespace).Delete(context.TODO(), k, metav1.DeleteOptions{})
+				if err != nil {
+					panic(err.Error())
+				}
 			}
 		}
 	}
